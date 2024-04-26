8dbdf3b0248761ec8ec2e8a498b367bc2dbe162d
Phobjai <phobthum.tarateeraseth@gmail.com>
2024-04-26 16:33:05 +0700

    add graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Phobjai/assessment-tax/tax"
 	"github.com/labstack/echo/v4"
@@ -17,5 +23,27 @@ func main() {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
 
-	e.Logger.Fatal(e.Start(":1323"))
+	port := os.Getenv("PORT")
+
+	go func() {
+		if err := e.Start(":" + port); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Doesn't block if no connections, but will otherwise wait until the timeout deadline.
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	//
+
+	fmt.Println("bye bye")
 }
